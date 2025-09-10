93ca4dd1555a22e61f465c2b0724b8c4b936c5da
Maria Guillen <mguillen@catalyte.io>
2025-09-10 13:22:04 -0500

    created more functions

--- FunctionsActivity.py
+++ FunctionsActivity.py
@@ -1,6 +1,29 @@
+def send_invites(): 
+    print("Send out invitations to all the guests")
+    
 def clean_up_house(): 
     print("Clean up entire house and organize")
 
 def grocery_shopping(): 
     print("Go buy all groceries for the food and snacks")
 
+def buy_decorations(): 
+    print("Buy all decorations for the party")
+
+def set_up_tables(): 
+    print("Set up tables and chairs for guests")
+
+def decorate_house():
+    print("Decorate the house")
+
+def make_food(): 
+    print("Make my favorite food to share with guets")
+
+def cleanup_kitchen():
+    print("Clean up kitchen after making food")
+
+def set_up_food():
+    print("Set up food and snack tables, including drinks")
+
+def play_music():
+    print("Play music and get ready for guests")
